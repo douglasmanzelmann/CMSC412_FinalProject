ed904d0da0c445bbc4c962fa619465ce599417ea
Douglas Manzelmann <dougamanzelmann@gmail.com>
2015-07-24 16:49:21 -0400

    Changed to ScheduledThreadExecutor

--- src/FCFSScheduler.java
+++ src/FCFSScheduler.java
@@ -1,6 +1,7 @@
 import java.util.Queue;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 /**
  * Created by test on 7/22/2015.
@@ -8,11 +9,21 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class FCFSScheduler implements Scheduler {
     private Queue<Process> readyQueue;
     private Queue<Process> ioQueue;
-    private ExecutorCompletionService<Process> workerThreads;
-    private ExecutorCompletionService<Process> ioThreads;
+    private ScheduledThreadPoolExecutor workerThreads;
+    private ScheduledThreadPoolExecutor ioThreads;
+    private ScheduledThreadPoolExecutor intermediateThreads;
+    private final int NUMBER_OF_WORKER_THREADS;
+    private final int NUMBER_OF_IO_THREADS;
+    private final int NUMBER_OF_INTERMEDIATE_THREADS;
 
     public FCFSScheduler() {
+        NUMBER_OF_WORKER_THREADS = 6;
+        NUMBER_OF_IO_THREADS = 1;
+        NUMBER_OF_INTERMEDIATE_THREADS = 2;
 
+        workerThreads = new ScheduledThreadPoolExecutor(NUMBER_OF_WORKER_THREADS);
+        ioThreads = new ScheduledThreadPoolExecutor(NUMBER_OF_IO_THREADS);
+        intermediateThreads = new ScheduledThreadPoolExecutor(NUMBER_OF_INTERMEDIATE_THREADS);
     }
 
     public boolean addToReadyQueue(Process process) {
